607d75f9cc6fbff828e4d6dc73427e12694b3df1
andrey <a.i.maslov199907@bk.ru>
2020-11-13 09:53:46 +0300

    добавил новое свойство дедлайну - id группы, в которой он был создан

--- src/main/java/com/company/models/Deadline.java
+++ src/main/java/com/company/models/Deadline.java
@@ -22,6 +22,7 @@ public class Deadline {
     @ToString.Exclude
     @Getter private long id;
     @Getter private final long creatorId;
+    @Getter private final Long groupId;
     @Getter @Setter private LocalDateTime creationDateTime;
     @Getter @Setter private LocalDateTime dateTime;
     @Getter @Setter private String title;
@@ -29,26 +30,25 @@ public class Deadline {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
     public Deadline(@JsonProperty(value = "creatorId") long creatorId,
+                    @JsonProperty(value = "groupId", defaultValue = "null") Long groupId,
                     @JsonProperty(value = "dateTime") String dateTime,
                     @JsonProperty(value = "title") String title,
                     @JsonProperty(value = "description") String description) {
-        this(creatorId, LocalDateTime.now(), LocalDateTime.parse(dateTime, formatter), title, description);
+        this(creatorId, groupId, LocalDateTime.now(), LocalDateTime.parse(dateTime, formatter), title, description);
     }
 
     protected Deadline() {
         creatorId = -1;
+        groupId = -1L;
     }
 
-    public Deadline(long creatorId, LocalDateTime creationDateTime, LocalDateTime dateTime,
+    public Deadline(long creatorId, Long groupId, LocalDateTime creationDateTime, LocalDateTime dateTime,
                     String title, String description) {
         this.creatorId = creatorId;
+        this.groupId = groupId;
         this.creationDateTime = creationDateTime;
         this.dateTime = dateTime;
         this.title = title;
         this.description = description;
     }
 }
-
-/*
-{ creatorId: 0, dateTime: "2020-11-15", title: "tmp", description="tmp description" }
- */
