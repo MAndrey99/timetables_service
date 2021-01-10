efa3bad101e8cb133426349cd2c2a559dea35637
andrey <a.i.maslov199907@bk.ru>
2021-01-10 22:07:26 +0300

    Переход на автоматическое преобразование json. initBinder вынесен из контроллера в настройки.

--- src/main/java/com/company/ServingWebContentApplication.java
+++ src/main/java/com/company/ServingWebContentApplication.java
@@ -1,19 +1,24 @@
 package com.company;
 
+import com.company.models.Deadline;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 
 import javax.annotation.PostConstruct;
+import java.beans.PropertyEditorSupport;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.TimeZone;
@@ -47,5 +52,10 @@ public class ServingWebContentApplication {
                 .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .featuresToEnable(JsonParser.Feature.ALLOW_SINGLE_QUOTES)
                 .build();
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder, @Autowired ObjectMapper jsonMapper) {
+        binder.registerCustomEditor(Deadline.class, new PropertyEditorSupport(jsonMapper));
     }
 }
--- src/main/java/com/company/controllers/DeadlineController.java
+++ src/main/java/com/company/controllers/DeadlineController.java
@@ -3,72 +3,64 @@ package com.company.controllers;
 import com.company.models.Deadline;
 import com.company.repositories.DeadlineRepository;
 import com.company.subscription.Notifier;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.beans.PropertyEditorSupport;
 import java.time.LocalDateTime;
 
 @RestController()
 @RequestMapping("deadlines")
 public class DeadlineController {
-    @Autowired private ObjectMapper objectMapper;
     @Autowired private DeadlineRepository deadlineRepository;
     @Autowired private Notifier notifier;
 
     @PostMapping
-    String postDeadline(@RequestBody Deadline deadline) throws JsonProcessingException {
+    Deadline postDeadline(@RequestBody Deadline deadline) {
         deadline = deadlineRepository.save(deadline);
         notifier.updateNextNotificationTime();
-        return objectMapper.writeValueAsString(deadline);
+        return deadline;
     }
 
     @PatchMapping(path = "/{id}")
-    String patchDeadline(@PathVariable long id, @RequestBody Deadline.DeadlinePatch patch)
-            throws JsonProcessingException {
+    Deadline patchDeadline(@PathVariable long id, @RequestBody Deadline.DeadlinePatch patch) {
         var deadline = deadlineRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         deadline.applyPatch(patch);
         deadlineRepository.save(deadline);
-        return objectMapper.writeValueAsString(deadline);
+        return deadline;
     }
 
     @GetMapping
-    String getDeadline(@RequestParam(value = "groupId", required = false) Long groupId,
+    Deadline.DeadlinesBucket getDeadline(@RequestParam(value = "groupId", required = false) Long groupId,
                        @RequestParam(value = "creatorId", required = false) Long creatorId,
-                       @RequestParam(defaultValue = "true") boolean relevant) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(
-                new Deadline.DeadlinesBucket(
-                        deadlineRepository.findAll((r, cq, cb) -> {
-                            cq.orderBy(cb.asc(r.get("dateTime")));
-                            var res = cb.conjunction();
-                            if (groupId != null) {
-                                res = cb.and(res, cb.equal(r.get("groupId"), groupId));
-                            }
-                            if (creatorId != null) {
-                                res = cb.and(res, cb.equal(r.get("creatorId"), creatorId));
-                            }
-                            if (relevant)
-                                res = cb.and(res, cb.greaterThan(r.get("dateTime"), LocalDateTime.now()));
-                            return res;
-                        })
-                )
-        );
+                       @RequestParam(defaultValue = "true") boolean relevant) {
+        return new Deadline.DeadlinesBucket(
+                    deadlineRepository.findAll((r, cq, cb) -> {
+                        cq.orderBy(cb.asc(r.get("dateTime")));
+                        var res = cb.conjunction();
+                        if (groupId != null) {
+                            res = cb.and(res, cb.equal(r.get("groupId"), groupId));
+                        }
+                        if (creatorId != null) {
+                            res = cb.and(res, cb.equal(r.get("creatorId"), creatorId));
+                        }
+                        if (relevant)
+                            res = cb.and(res, cb.greaterThan(r.get("dateTime"), LocalDateTime.now()));
+                        return res;
+                    })
+            );
     }
 
     @GetMapping("/{id}")
-    String getDeadline(@RequestParam(value = "groupId", required = false) Long groupId,
-                       @PathVariable(value = "id") long id) throws JsonProcessingException {
+    Deadline getDeadline(@RequestParam(value = "groupId", required = false) Long groupId,
+                       @PathVariable(value = "id") long id) {
         var d = deadlineRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         if (groupId != null && d.getGroupId() != groupId)
             throw new ResponseStatusException(HttpStatus.EXPECTATION_FAILED);
-        return objectMapper.writeValueAsString(d);
+        return d;
     }
 
     @DeleteMapping("/{id}")
@@ -79,10 +71,5 @@ public class DeadlineController {
         if (groupId != null && deadline.getGroupId() != groupId)
             throw new ResponseStatusException(HttpStatus.EXPECTATION_FAILED);
         deadlineRepository.delete(deadline);
-    }
-
-    @InitBinder
-    public void initBinder(WebDataBinder binder) {
-        binder.registerCustomEditor(Deadline.class, new PropertyEditorSupport(objectMapper));
     }
 }
