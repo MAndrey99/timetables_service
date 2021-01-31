6e4ada371e1ee886187830e4738ac54cecc74e35
andrey <a.i.maslov199907@bk.ru>
2021-01-31 13:21:08 +0300

    сделал Schedulers потокобезопасным

--- src/main/java/com/company/scheduling/Schedulers.java
+++ src/main/java/com/company/scheduling/Schedulers.java
@@ -4,24 +4,19 @@ import com.company.models.Deadline;
 import com.company.models.Schedule;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.TreeMap;
 
 public final class Schedulers {
-    static Map<String, Scheduler> schedulers = new TreeMap<>();
-    static {
-        schedulers.put("SRTF", new SRTFScheduler());
-        schedulers.put("prioritySRTF", new PrioritySRTFScheduler());
-    }
-
     private Schedulers() {}
 
     public static Optional<Schedule> schedule(List<Deadline> deadlineList, String schedulerName) {
-        var scheduler = schedulers.get(schedulerName);
-        if (scheduler == null)
-            throw new UnsupportedOperationException("алгоритм планирования " + schedulerName + " не поддерживается");
-
+        var scheduler = switch (schedulerName) {
+            case "SRTF" -> new SRTFScheduler();
+            case "prioritySRTF" -> new PrioritySRTFScheduler();
+            default -> throw new UnsupportedOperationException(
+                    "алгоритм планирования %s не поддерживается".formatted(schedulerName)
+            );
+        };
         scheduler.setDeadlines(deadlineList);
         return scheduler.getSchedule();
     }
